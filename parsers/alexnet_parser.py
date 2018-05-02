53fbd46071012f6fa1b67d97a061d874260d1d56
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-05-02 14:59:48 -0500

    Get final log graph

--- parsers/alexnet_parser.py
+++ parsers/alexnet_parser.py
@@ -3,25 +3,26 @@ import matplotlib.pyplot as plt
 import re
 import csv
 
-INPUT = "../logs/alexnet-training.log"
+INPUTS = ["../logs/alexnet-training.log", "../logs/alexnet-training-2.log"]
 
 def parse_logfile():
 
     iterations = []
     losses = []
-    with open(INPUT) as file:
-        for line in file:
-            match_obj = re.search(r"Iteration [+-]?\d+(?:\.\d+)?", line)
-            if match_obj:
-                group_items = match_obj.group(0).split(' ')
-                iter_num = group_items[1]
-                iterations.append(int(iter_num))
+    for input in INPUTS:
+        with open(input) as file:
+            for line in file:
+                match_obj = re.search(r"Iteration [+-]?\d+(?:\.\d+)?", line)
+                if match_obj:
+                    group_items = match_obj.group(0).split(' ')
+                    iter_num = group_items[1]
+                    iterations.append(int(iter_num))
 
-            match_obj = re.search(r"loss = [+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?", line)
-            if match_obj:
-                group_items = match_obj.group(0).split(' ')
-                loss = group_items[2]
-                losses.append(float(loss))
+                match_obj = re.search(r"loss = [+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?", line)
+                if match_obj:
+                    group_items = match_obj.group(0).split(' ')
+                    loss = group_items[2]
+                    losses.append(float(loss))
 
     _iterations = []
     _losses = []
@@ -33,50 +34,22 @@ def parse_logfile():
 
     return _iterations, _losses
 
-def get_accuracies():
-    iterations = []
-    accuracies = []
-    with open(INPUT) as file:
-        for line in file:
-            match_obj = re.search(r"Iteration [+-]?\d+(?:\.\d+)?", line)
-            if match_obj:
-                group_items = match_obj.group(0).split(' ')
-                iter_num = group_items[1]
-                iterations.append(int(iter_num))
-
-            match_obj = re.search(r"loss = [+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?", line)
-            if match_obj:
-                group_items = match_obj.group(0).split(' ')
-                accuracy = group_items[2]
-                accuracies.append(float(loss))
-
-    _iterations = []
-    _accuracies = []
-    for i in range(len(iterations)):
-        iter = iterations[i]
-        if iter not in _iterations:
-            _iterations.append(iter)
-            _accuracies.append(accuracies[i])
-
-    return _iterations, _accuracies
-
 def graph_logfile(iterations, losses):
     plt.plot(iterations, losses)
-    plt.savefig(INPUT + "-graph.png")
-
-def graph_logfile(iterations, accuracies):
-    plt.plot(iterations, accuracies)
-    plt.savefig(INPUT + "-accuracies-graph.png")
+    plt.ylabel('loss')
+    plt.xlabel('iterations')
+    plt.savefig(INPUTS[0] + "-graph.png")
 
 def write_to_file(iterations, losses):
-    with open(INPUT + "_parsed", "wb") as outfile:
+    with open(INPUTS[0] + "_parsed", "wb") as outfile:
         csv_writer = csv.writer(outfile, delimiter=",")
         for i,iteration in enumerate(iterations):
             csv_writer.writerow((iteration, losses[i]))
 
 def main():
-    iterations, accuracies = get_accuracies()
-    graph_accuracies(iterations, accuracies)
+    # iterations, losses = parse_logfile()
+    # graph_logfile(iterations, losses)
+    # write_to_file(iterations, losses)
 
 
 if __name__ == '__main__':
