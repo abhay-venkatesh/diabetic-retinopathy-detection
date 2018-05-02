0f7bdebc6feb0c5f16555a73cfc08db641c75b4f
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-05-01 21:43:35 -0500

    Wait for final results

--- parsers/alexnet_parser.py
+++ parsers/alexnet_parser.py
@@ -33,9 +33,40 @@ def parse_logfile():
 
     return _iterations, _losses
 
+def get_accuracies():
+    iterations = []
+    accuracies = []
+    with open(INPUT) as file:
+        for line in file:
+            match_obj = re.search(r"Iteration [+-]?\d+(?:\.\d+)?", line)
+            if match_obj:
+                group_items = match_obj.group(0).split(' ')
+                iter_num = group_items[1]
+                iterations.append(int(iter_num))
+
+            match_obj = re.search(r"loss = [+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?", line)
+            if match_obj:
+                group_items = match_obj.group(0).split(' ')
+                accuracy = group_items[2]
+                accuracies.append(float(loss))
+
+    _iterations = []
+    _accuracies = []
+    for i in range(len(iterations)):
+        iter = iterations[i]
+        if iter not in _iterations:
+            _iterations.append(iter)
+            _accuracies.append(accuracies[i])
+
+    return _iterations, _accuracies
+
 def graph_logfile(iterations, losses):
     plt.plot(iterations, losses)
     plt.savefig(INPUT + "-graph.png")
+
+def graph_logfile(iterations, accuracies):
+    plt.plot(iterations, accuracies)
+    plt.savefig(INPUT + "-accuracies-graph.png")
 
 def write_to_file(iterations, losses):
     with open(INPUT + "_parsed", "wb") as outfile:
@@ -44,9 +75,8 @@ def write_to_file(iterations, losses):
             csv_writer.writerow((iteration, losses[i]))
 
 def main():
-    iterations, losses = parse_logfile()
-    graph_logfile(iterations, losses)
-    write_to_file(iterations, losses)
+    iterations, accuracies = get_accuracies()
+    graph_accuracies(iterations, accuracies)
 
 
 if __name__ == '__main__':
